40182822cbdbe8fe0a0f5ff1ee299ca5cc9019e0
Summma <pinto.alex05@gmail.com>
2023-12-05 19:49:56 -0500

    Pushed changes.

--- ResNetDetection.py
+++ ResNetDetection.py
@@ -112,9 +112,60 @@ def ResNet152(img_channels=3, num_classes=1000):
 
 
 def test():
-    net = ResNet50(img_channels=1, num_classes=10).to('cpu')
-    x = torch.randn(2, 1, 28, 28)
-    model = net(x).to('cpu')
+    transform_train = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+
+    trainloader = torch.utils.data.DataLoader(train, batch_size=128, shuffle=True, num_workers=2)
+
+    test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+
+    testloader = torch.utils.data.DataLoader(test, batch_size=128,shuffle=False, num_workers=2)
+
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    model = ResNet50(img_channels=1, num_classes=10).to('cpu')
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.1, patience=5)
+
+    EPOCHS = 10
+    for epoch in range(EPOCHS):
+        losses = []
+        running_loss = 0
+        for i, inp in enumerate(trainloader):
+            inputs, labels = inp
+            inputs, labels = inputs.to('cpu'), labels.to('cpu')
+            optimizer.zero_grad()
+        
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            losses.append(loss.item())
+
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            
+            if i%100 == 0 and i > 0:
+                print(f'Loss [{epoch+1}, {i}](epoch, minibatch): ', running_loss / 100)
+                running_loss = 0.0
+
+        avg_loss = sum(losses)/len(losses)
+        scheduler.step(avg_loss)
+            
+    print('Training Done')
 
 
 if __name__ == '__main__':
