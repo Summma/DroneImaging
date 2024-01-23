7853d0011d0412c99ec7bee5dbb19fda31e441ce
Summma <pinto.alex05@gmail.com>
2024-01-23 12:24:19 -0500

    Added some docstrings to the ShapeBox code

--- ShapeBox.py
+++ ShapeBox.py
@@ -20,14 +20,50 @@ class DetectShape:
         cv.waitKey()
     
     def grey_image(self):
+        """Multiplies values in image and converts it to greyscale
+
+        AddWeighted
+            - addWeighted is used to perform a weighted sum of two images, but in this case, we're just using it
+            - to multiply certain values in the image to make them more prominent once it's greyscaled.
+        cvtColor
+            - This is just greyscaling the image.
+        """
+
         self.image = cv.addWeighted(self.image, 2.5, self.image, 0, 0.2)
         self.image = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
     
     def blur_image(self):
+        """Applies a blur
+
+        Used to be implemented using a gaussian blur, but I've found that for detecting shapes in a difficult
+        environment, a bilateral filter is better suited because it's more catered towards perserving edges
+        and reducing any noise in the image. More sophisticated algorithms exist to do the same thing, but
+        they're far slower.
+        """
+
         # self.image = cv.GaussianBlur(self.image, (5, 5), 0)
         self.image = cv.bilateralFilter(self.image, 15, 75, 75)
     
     def edge_detection(self):
+        """Edge Detection
+
+        While the bilateral filter, in a lot of cases, manages to detect most edges out of the box, it's good
+        and not all that time consuming to still apply edge detection to make edges even more prominent.
+
+        *Note* In the implementation of cv.Canny, a gaussian filter is used to reduce noise.
+
+        This method works as follows:
+            1. Find median of all pixel values
+            2. Set a sigma value (Explained later)
+            3. Define two thresholds. In the implementation of cv.Canny, thresholds work as follows
+                - Values above threshold_two are edges
+                - Values below threshold_one are not edges
+                - Values in between the two are only edges if connected to an already establish edge.
+                - Sigma is just used to adjust the threshold. I played around with it for a bit and
+                  found that around 0.8 is fine. There's no real reason for it, it just works well.
+            4. Finally, cv.Canny takes care of edge detection using these parameters.
+        """
+
         v = np.median(self.image)
         sigma = 0.8
 
@@ -37,6 +73,28 @@ class DetectShape:
         self.image = cv.Canny(self.image, threshold_one, threshold_two)
 
     def clean_up(self, dilation_iterations=2, erode_iterations=1):
+        """Cleans up image by applying erosion and dilation
+
+        This should be used exclusively after applying edge detection, or else it doesn't do a lot.
+
+        DILATE
+        --------
+        Dilate, after edge detection, will make all lines bigger. I do this to remove and gaps in shapes that we
+        detect.
+
+        ERODE
+        --------
+        Erosion, after edge detection, will make lines thinner. This is applied after dilation to reduce the size
+        of the lines again to normal. After completing these operations in succession, lines that should be connected
+        will have a higher chance of actually being connected. 
+
+        *Note* (This can also introduce fake shapes as well)
+
+        KERNEL
+        --------
+        This determines the size of the matrix that scans over the image and applies the erode and dilate filters.
+        """
+
         kernel = np.ones((4, 5), dtype=np.uint8)
         d_im = cv.dilate(self.image, kernel, iterations=dilation_iterations)
         self.image = cv.erode(d_im, kernel, iterations=erode_iterations)
